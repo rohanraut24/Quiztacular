c91bca618c2a1de885c7ebd1668550fa26b09903
CodeSage <zaidshaikh2619@gmail.com>
2025-10-24 17:32:16 +0530

    initial setup for all services

---
+++ quizz-service/src/test/java/quizz_service/QuizzServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package quizz_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
