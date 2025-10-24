144fd508d9b27886504ecea78f11f1582f87e19e
CodeSage <zaidshaikh2619@gmail.com>
2025-10-24 18:04:49 +0530

    service registry added

---
+++ service-registry/src/test/java/service_registry/ApplicationTests.java
@@ -0,0 +1,13 @@
+package service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
