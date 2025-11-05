a9d1aec81e72b5e98a79311f904dfa59d18e4f54
rohan raut <rohanraut2443@gmail.com>
2025-11-05 13:10:30 +0530

    add remaining files

---
+++ quiz-room-service/src/main/java/rohan/quizroom/QuizRoomServiceApplication.java
@@ -0,0 +1,13 @@
+package rohan.quizroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizRoomServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizRoomServiceApplication.class, args);
+	}
+
+}
---
+++ quiz-room-service/src/test/java/rohan/quizroom/QuizRoomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rohan.quizroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizRoomServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
